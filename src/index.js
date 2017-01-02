68905a45622eb4c849512451038de406bc75ae75
Harri Huhtala <hmh.uhtala11@gmail.com>
2017-01-02 22:30:30 +0200

    es6 class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import Dog from './dog';
+
 const str = 'ES6';
-console.log(`Hello ${str}`);
 console.log(`Hello before declaring const: ${hoist}`);
 const hoist = 'hoist';
+const toby = new Dog('Toby');
+
+
+console.log(`Hello ${str}`);
 console.log(`Hello after declaration: ${hoist}`);
+console.log(toby.bark());
