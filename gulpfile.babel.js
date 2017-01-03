2babdbb6ace63f13f7082c20ea9eedf760467afe
Harri Huhtala <hmh.uhtala11@gmail.com>
2017-01-03 21:52:48 +0200

    eslint airbnb config setup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,32 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable Unexpected block statement surrounding arrow body */
+
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import del from 'del';
-import exec from 'child_process';
+import { exec } from 'child_process';
+import eslint from 'gulp-eslint';
 
 const paths = {
   allSrcJs: 'src/**/*.js',
-  libDir: 'lib'
+  gulpFile: 'gulpfile.babel.js',
+  libDir: 'lib',
 };
 
-gulp.task('clean', () =>  {
-  return del(paths.libDir);
-});
+gulp.task('clean', () => del(paths.libDir));
 
-gulp.task('build', () => {
-  return gulp.src(paths.allSrcJs)
+gulp.task('build', ['lint', 'clean'], () =>
+  gulp.src(paths.allSrcJs)
     .pipe(babel())
-    .pipe(gulp.dest(paths.libDir));
+    .pipe(gulp.dest(paths.libDir)));
+
+gulp.task('main', ['build'], (callback) => {
+  exec(`node ${paths.libDir}`, (error, stdout) => {
+    console.log(stdout);
+    return callback(error);
+  });
 });
 
-gulp.task('main', ['build'], (callback) =>{
-  exec(`node ${paths.libDir}`, (error,stdout) =>{
-    console.log(stdout);
-    return callback(error)
-  })
-});
+gulp.task('lint', () =>
+  gulp.src([
+    paths.allSrcJs,
+    paths.gulpFile,
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError()));
 
 gulp.task('watch', () => {
   gulp.watch(paths.allSrcJs, ['main']);
 });
 
-gulp.task('default', ['watch','main']);
+gulp.task('default', ['watch', 'main']);
